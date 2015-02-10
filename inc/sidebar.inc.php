c882425de08b43e1081aa449a17dacd6fff91959
tarik817 <tarik817@gmail.com>
2015-02-10 15:39:29 +0200

    Start working on multilingual.

--- inc/sidebar.inc.php
+++ inc/sidebar.inc.php
@@ -2,6 +2,9 @@
 <a class="bott" href="user.php?display">Users</a><br>
 <?php
 if(isset($_SESSION['e_user'])){
-echo '<a href="add.php" class="bott">Add news</a>';	
-} 
+echo '<a href="add.php" class="bott">Add news</a><br>';	
+}
+if(isset($_SESSION['admin'])){
+	echo '<a href="lang.php" class="bott">Add translation</a><br>';	
+}
 
